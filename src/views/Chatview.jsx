50a0eabeed055586344311541b2eb50117666807
jrdanDp <134174675+jrdanDp@users.noreply.github.com>
2025-12-12 18:14:13 -0500

    Update Chatview.jsx

--- src/views/Chatview.jsx
+++ src/views/Chatview.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import Header from '../components/Common/Header';
 import SecurityBadge from '../components/Common/SecurityBadge';
 import Chat from '../components/Chat/Chat.jsx';
@@ -17,20 +17,7 @@ const ChatView = () => {
         <span className="security-icon">🔒</span>
         <span>Tus conversaciones no se guardan. SerenBot es completamente anónimo.</span>
       </footer>
-=======
-import Chat from '../components/Chat/Chat.jsx';
-import '../styles/views.css';
-
-const ChatView = () => {
-
-  return (
-    <div className="chat-view-container">
-
-      <header className="chat-header">
-        <h1>SerenBot</h1>
-      </header>
-      <Chat />
->>>>>>> 3ba1d143ba0c1fdab7309cb55ed369554f725765
+      
     </div>
   );
 };

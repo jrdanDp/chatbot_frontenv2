891a2365968493998ce9065215f0bd263977e402
jrdanDp <134174675+jrdanDp@users.noreply.github.com>
2025-12-12 18:18:06 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
+
+/* Estilos Globales (Variables y Reset) */
 import './styles/theme.css';
 import './styles/base.css';
+
+/* Estilos de Componentes y Utilidades */
 import './styles/components.css';
+import './styles/animation.css';
+
+/* Estilos de Vistas */
+import './styles/views.css';
 import './styles/home.css';
 import './styles/chat-view.css';
-=======
-import './styles/base.css';
-import './styles/components.css';
->>>>>>> 3ba1d143ba0c1fdab7309cb55ed369554f725765
-import './styles/views.css';
-import './styles/animation.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
